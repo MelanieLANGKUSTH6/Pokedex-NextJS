9b76143267e19873856f190028d7d123c16da4c8
MelanieLANGKUSTH6 <melanie.langkusth@oclock.school>
2023-09-12 11:01:24 +0200

    ajout interface pour les data pokemons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,27 @@
-'use client';
+import { Pokemon } from '@/@types/pokemon';
 
-import { useEffect } from 'react';
+const getData = async () => {
+  const responseJson = await fetch('https://api-pokemon-fr.vercel.app/api/v1/pokemon');
+  const response = await responseJson.json();
+  return response as Pokemon[];
+};
 
-export default function Home() {
-  useEffect(() => {
-    const getData = async () => {
-      const responseJson = await fetch('https://api-pokemon-fr.vercel.app/api/v1/pokemon');
-      const response = await responseJson.json();
-      console.log(response);
-    };
-    getData();
-  }, []);
+export default async function Home() {
+  const data = await getData();
+  const first10pokemons = data.slice(1, 10);
 
   return (
     <main className="min-h-screen p-4">
       <h2 className="text-red-600 hover:text-yellow-400">
         Pokedex
       </h2>
-      <p className="">
-        Nos pokémons
-      </p>
+      <div className="">
+        {
+          first10pokemons.map((pokemon) => (
+            <div key={pokemon.pokedexId}>{pokemon.name.fr}</div>
+          ))
+        }
+      </div>
     </main>
   );
 }
