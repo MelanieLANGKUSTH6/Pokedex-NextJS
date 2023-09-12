4f6bca39c46871cf97de792370c92aa0b96a5da3
MelanieLANGKUSTH6 <melanie.langkusth@oclock.school>
2023-09-12 10:19:21 +0200

    use client avec appel API

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+export default function About() {
+  return (
+    <main className="min-h-screen p-4">
+      <h2 className="text-red-600 hover:text-yellow-400">
+        About
+      </h2>
+      <p className="">
+        Dev à la recherche d&apos;un stage
+      </p>
+    </main>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,17 @@
+'use client';
+
+import { useEffect } from 'react';
+
 export default function Home() {
+  useEffect(() => {
+    const getData = async () => {
+      const responseJson = await fetch('https://api-pokemon-fr.vercel.app/api/v1/pokemon');
+      const response = await responseJson.json();
+      console.log(response);
+    };
+    getData();
+  }, []);
+
   return (
     <main className="min-h-screen p-4">
       <h2 className="text-red-600 hover:text-yellow-400">
