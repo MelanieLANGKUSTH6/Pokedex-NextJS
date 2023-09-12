838ec5481f7a4a915ef9be396532e74819335155
MelanieLANGKUSTH6 <melanie.langkusth@oclock.school>
2023-09-12 10:01:38 +0200

    Mise en place html

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="text-pink-500 hover:text-cyan-400">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-      </div>
+    <main className="min-h-screen p-4">
+      <h2 className="text-red-600 hover:text-yellow-400">
+        Pokedex
+      </h2>
+      <p className="">
+        Nos pokémons
+      </p>
     </main>
   );
 }
